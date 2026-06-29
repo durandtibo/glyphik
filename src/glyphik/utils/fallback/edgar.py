7807855e86bb807ed8cdceafbac320238d75c764
Thibaut Durand <durand.tibo@gmail.com>
2026-06-29 13:38:19 -0700

    Add edgar fallback (#37)

---
+++ src/glyphik/utils/fallback/edgar.py
@@ -0,0 +1,38 @@
+r"""Contain fallback implementations used when ``edgar`` dependency is
+not available."""
+
+from __future__ import annotations
+
+__all__ = ["Company", "CompanyNotFoundError", "edgar"]
+
+from types import ModuleType
+from typing import Any
+
+from glyphik.utils.imports import raise_edgar_missing_error
+
+
+class FakeClass:
+    r"""Fake class that raises an error because edgar is not installed.
+
+    Args:
+        *args: Positional arguments.
+        **kwargs: Keyword arguments.
+
+    Raises:
+        RuntimeError: edgar is required for this functionality.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ARG002
+        raise_edgar_missing_error()
+
+
+Company = FakeClass
+CompanyNotFoundError = FakeClass
+
+entity: ModuleType = ModuleType("edgar.entity")
+entity.CompanyNotFoundError = CompanyNotFoundError
+
+# Create a fake edgar package
+edgar: ModuleType = ModuleType("edgar")
+edgar.entity = entity
+edgar.Company = Company
