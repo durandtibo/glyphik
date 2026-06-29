da23746eaa1f045e34630b2e760814941db85508
Thibaut Durand <durand.tibo@gmail.com>
2026-06-29 16:00:19 -0700

    Add `fetch_filings`

--- src/glyphik/data/sec/__init__.py
+++ src/glyphik/data/sec/__init__.py
@@ -2,7 +2,15 @@ r"""Contain code to prepare SEC data."""
 
 from __future__ import annotations
 
-__all__ = ["SecForm", "fetch_ticker_from_cik"]
+__all__ = [
+    "SecFilingRecord",
+    "SecForm",
+    "fetch_filings",
+    "fetch_form_filings",
+    "fetch_ticker_from_cik",
+]
+
 
 from glyphik.data.sec.cik import fetch_ticker_from_cik
+from glyphik.data.sec.filing import SecFilingRecord, fetch_filings, fetch_form_filings
 from glyphik.data.sec.form import SecForm
