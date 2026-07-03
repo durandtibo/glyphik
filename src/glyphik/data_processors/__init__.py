f0af746f88950463f26810909d40c6ae6422bcff
Thibaut Durand <durand.tibo@gmail.com>
2026-07-03 10:17:28 -0700

    Add `RedownloadSecFilingProcessor`

--- src/glyphik/data_processors/__init__.py
+++ src/glyphik/data_processors/__init__.py
@@ -2,8 +2,9 @@ r"""Contain data processors."""
 
 from __future__ import annotations
 
-__all__ = ["SecFilingRecordToDocumentProcessor"]
+__all__ = ["RedownloadSecFilingProcessor", "SecFilingRecordToDocumentProcessor"]
 
+from glyphik.data_processors.redownload_sec_filing import RedownloadSecFilingProcessor
 from glyphik.data_processors.sec_record_to_document import (
     SecFilingRecordToDocumentProcessor,
 )
