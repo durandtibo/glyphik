fc093598a4812923cf11547d54bb0df38282ad2f
Thibaut Durand <durand.tibo@gmail.com>
2026-07-04 12:12:12 -0700

    Add `SecFilingIngestor` (#114)

--- src/glyphik/ingestors/__init__.py
+++ src/glyphik/ingestors/__init__.py
@@ -6,12 +6,14 @@ __all__ = [
     "DocumentStoreIndexingIngestor",
     "SecCompanyIngestor",
     "SecFilingDocumentStoreIngestor",
+    "SecFilingIngestor",
     "Sp1500CompanyIngestor",
     "Sp1500FilingIngestor",
 ]
 
 from glyphik.ingestors.document_store_indexing import DocumentStoreIndexingIngestor
 from glyphik.ingestors.sec_company import SecCompanyIngestor
+from glyphik.ingestors.sec_filing import SecFilingIngestor
 from glyphik.ingestors.sec_filing_document_store import SecFilingDocumentStoreIngestor
 from glyphik.ingestors.sp1500_company import Sp1500CompanyIngestor
 from glyphik.ingestors.sp1500_filing import Sp1500FilingIngestor
