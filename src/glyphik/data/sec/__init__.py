6c4b718f63a485c993263cb2ad6e5170b8aae058
Thibaut Durand <durand.tibo@gmail.com>
2026-07-04 11:59:50 -0700

    Add `cache_or_fetch_filings`

--- src/glyphik/data/sec/__init__.py
+++ src/glyphik/data/sec/__init__.py
@@ -11,10 +11,15 @@ __all__ = [
     "fetch_filings",
     "fetch_form_filings",
     "fetch_ticker_from_cik",
+    "load_or_fetch_company_filings",
+    "load_or_fetch_filings",
     "normalize_content_format",
 ]
 
-
+from glyphik.data.sec.cache_or_fetch_filings import (
+    load_or_fetch_company_filings,
+    load_or_fetch_filings,
+)
 from glyphik.data.sec.cik import fetch_ticker_from_cik
 from glyphik.data.sec.company import CompanyIdentifier
 from glyphik.data.sec.filing import fetch_filings, fetch_form_filings
