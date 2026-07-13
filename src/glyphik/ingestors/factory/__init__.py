52abe89cc22a86ab462dfb12cc283e72d9f130d6
Thibaut Durand <durand.tibo@gmail.com>
2026-07-12 20:53:13 -0700

    Add `SecFilingIngestorFactory` (#186)

---
+++ src/glyphik/ingestors/factory/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain ingestor factories."""
+
+from __future__ import annotations
+
+__all__ = ["SecFilingIngestorFactory"]
+
+from glyphik.ingestors.factory.sec_filing import SecFilingIngestorFactory
